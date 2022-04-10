160fc1e59061e9fcc7002d17f72da1cbc3653a67
ggl <ggl@163.com>
2022-04-11 00:30:18 +0800

    初始化应用

---
+++ src/test/java/com/example/gitprojtest/GitProjTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitprojtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitProjTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
